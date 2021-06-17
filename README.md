c76442b05c54125343ddda0a72a0ffba765ef3d6
Muhammad Asif Javed <axif.javed@gmail.com>
2021-06-17 13:02:58 -0700

    spotify playlist data fetched.

---
+++ README.md
@@ -0,0 +1,3 @@
+mainstreamUrl: https://open.spotify.com/playlist/37i9dQZF1DX0XUsuxWHRQd
+
+PromoterIds: https://open.spotify.com/playlist/37i9dQZF1DX4SrOBCjlfVi, https://open.spotify.com/playlist/37i9dQZF1DWW4igXXl2Qkp,https://open.spotify.com/playlist/6oZhNW8o5ru7mb4RFkWn0M,https://open.spotify.com/playlist/4EtswXAGuGuUQcW9ctRour,https://open.spotify.com/playlist/6hWMmrVlMTvME8u0KchOpa,https://open.spotify.com/playlist/0c6wMHB5HsuZUscv3PQpih,https://open.spotify.com/playlist/6e8MhEouOuoBRYnV9GuGtK,https://open.spotify.com/playlist/5L3vZ9scrlV9DAcDEagI4c,https://open.spotify.com/playlist/0UHup1TpaqtEUD3k8H6LG5
--- index.js
+++ index.js
@@ -1,42 +1,77 @@
 const inquirer = require("inquirer");
+const { isBefore, differenceInDays } = require("date-fns");
 
 const { setupSpotify, getIdFromLink, getPlaylistData } = require("./helper");
 
 var questions = [
-	{
-		type: "input",
-		name: "mainstream",
-		message: "Enter mainstream url 🔗)",
-	},
-	{
-		type: "input",
-		name: "promoters",
-		message: "Enter promoter's urls 🔗,🔗 Comma separated?",
-	},
+  {
+    type: "input",
+    name: "mainstream",
+    message: "Enter mainstream url 🔗)",
+  },
+  {
+    type: "input",
+    name: "promoters",
+    message: "Enter promoter's urls 🔗,🔗 Comma separated?",
+  },
 ];
 
 const main = async () => {
-	try {
-		inquirer.prompt(questions).then(async (answers) => {
-			const mainstreamUrl = answers["mainstream"];
-			const promotersUrl = answers["promoters"];
-			const promotersUrlWithoutSpace = (promotersUrl || "").replace(/\s/g, "");
-			const promotersUrlList = promotersUrlWithoutSpace.split(",");
+  try {
+    inquirer.prompt(questions).then(async (answers) => {
+      const mainstreamUrl = answers["mainstream"];
+      const promotersUrl = answers["promoters"];
+      const promotersUrlWithoutSpace = (promotersUrl || "").replace(/\s/g, "");
+      const promotersUrlList = promotersUrlWithoutSpace.split(",");
 
-			await setupSpotify();
-			const mainstreamPlaylist = await getPlaylistData(
-				getIdFromLink(mainstreamUrl)
-			);
+      await setupSpotify();
 
-			if (promotersUrlList.length) {
-				for (const item of promotersUrlList) {
-					const playlist = await getPlaylistData(getIdFromLink(item));
-				}
-			}
-		});
-	} catch (error) {
-		console.log("🚀 ~ file: index.js ~ line 34 ~ main ~ error", error);
-	}
+      let mainstreamTracks = {};
+
+      const mainstreamPlaylist = await getPlaylistData(
+        getIdFromLink(mainstreamUrl)
+      );
+
+      for (const item of mainstreamPlaylist?.tracks?.items) {
+        mainstreamTracks[item.track.uri] = item;
+      }
+
+      let totalPromoted = {};
+      let playlistTrackDuration = {};
+
+      if (promotersUrlList.length) {
+        for (const promoterUrl of promotersUrlList) {
+          const pId = getIdFromLink(promoterUrl);
+          totalPromoted[pId] = 0;
+          playlistTrackDuration[pId] = 0;
+          const playlist = await getPlaylistData(pId);
+          for (const item of playlist?.tracks?.items) {
+            if (
+              mainstreamTracks[item.track.uri] &&
+              isBefore(
+                new Date(mainstreamTracks[item.track.uri].added_at),
+                new Date(item.added_at)
+              )
+            ) {
+              totalPromoted[pId] = totalPromoted[pId] + 1;
+              const diffInDays = differenceInDays(
+                new Date(),
+                new Date(item.added_at)
+              );
+              if (playlistTrackDuration[pId] < diffInDays) {
+                playlistTrackDuration[pId] = diffInDays;
+              }
+            }
+          }
+        }
+      }
+
+      console.log("🚀 totalPromoted", totalPromoted);
+      console.log("🚀 ~playlistTrackDuration", playlistTrackDuration);
+    });
+  } catch (error) {
+    console.log("🚀 ~ file: index.js ~ line 34 ~ main ~ error", error);
+  }
 };
 
 main();
